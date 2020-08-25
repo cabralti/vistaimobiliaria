ff536a4fa98b4f20c8b8dcd3b9080290457684f1
Gabriel Cabral <cabral.9santos@gmail.com>
2020-08-25 16:03:06 -0300

    Redirecionamento para home

--- templates/vistaimobiliaria/layouts/partials/sidebar.php
+++ templates/vistaimobiliaria/layouts/partials/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link text-center">
+    <a href="<?= url('') ?>" class="brand-link text-center">
         <span class="brand-text font-weight-light">Vista Imobiliária</span>
     </a>
 
